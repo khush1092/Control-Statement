ea3686d4ebe7c33724851485518b85c525343385
Khush Shah <khush82004@gmail.com>
2025-03-30 19:35:06 +0530

    Add files via upload

---
+++ Even_Odd.py
@@ -0,0 +1,6 @@
+# Check number if it is seven or odd
+num = int(input("Enter Number"))
+if num % 2 == 0:
+    print("Number :",num,"is Even")
+else:
+    print("Number :", num, "is Odd")
---
+++ Sum_loop.py
@@ -0,0 +1,5 @@
+#Sum from 1 to 50 using loop
+Total = 0
+for i in range(1,51):
+    Total += i
+print("Number from 1 to 50 total is:",Total)
